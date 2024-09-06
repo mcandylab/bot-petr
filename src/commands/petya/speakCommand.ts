29629d8d6ce5b0c6bfd6f0a65c64254d0fc0d740
Andrey Abramov <an.abramov@internet.ru>
2024-09-06 18:56:24 +0300

    Рефакторинг

--- src/commands/petya/speakCommand.ts
+++ src/commands/petya/speakCommand.ts
@@ -14,11 +14,13 @@ export default class SpeakCommand {
   }
 
   private async execute(string: string): Promise<string> {
+    console.log(string);
     return await axios
       .post('https://chat.mcandylab.ru/chat', {
         text: string,
       })
       .then(({ data }) => {
+        console.log(data);
         return data.response;
       })
       .catch((reason) => {
