8cde325f7b46139121ab0a53386f8954583d0113
Andrey Abramov <an.abramov@internet.ru>
2024-09-06 21:51:21 +0300

    Рефакторинг

--- src/controllers/petyaController.ts
+++ src/controllers/petyaController.ts
@@ -5,6 +5,7 @@ import DailyPeopleCommand from '../commands/petya/dailyPeopleCommand';
 import RegistrationCommand from '../commands/petya/registrationCommand';
 import WhoCommand from '../commands/petya/whoCommand';
 import SpeakCommand from '../commands/petya/speakCommand';
+import * as repl from 'node:repl';
 
 export default class PetyaController {
   private choiceCommand: ChoiceCommand;
@@ -43,6 +44,19 @@ export default class PetyaController {
         } else if (text.startsWith('инфа')) {
           const command = text.substring(4).trim();
           await this.infoCommand.init(context, command);
+        } else if (text === 'удали неактивных') {
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+          await context.reply('Начинаю поиск неактивных...');
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+          await context.reply(
+            'Ага, попались! В следующий раз будьте активнее!',
+          );
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+          await context.telegram.banChatMember(-1002155745099, 457697191);
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+          await context.telegram.banChatMember(-1002155745099, 566282848);
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+          await context.reply('Хотя для вас уже следующего раза не будет!');
         } else {
           await this.speakCommand.init(context, text);
         }
