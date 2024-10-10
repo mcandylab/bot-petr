3f3b370e243a25a373a0f2b3dceba656f9570e27
Andrey Abramov <an.abramov@internet.ru>
2024-10-10 22:17:29 +0300

    Подключил OpenAI

--- src/commands/petya/speakCommand.ts
+++ src/commands/petya/speakCommand.ts
@@ -1,8 +1,16 @@
 import { Context } from 'telegraf';
-import axios from 'axios';
-import * as https from 'node:https';
+import OpenAI from 'openai';
 
 export default class SpeakCommand {
+  client: OpenAI;
+
+  constructor() {
+    this.client = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+      baseURL: 'https://api.proxyapi.ru/openai/v1/',
+    });
+  }
+
   public async init(context: Context, command: string): Promise<void> {
     if (!command) {
       await context.reply('Шо');
@@ -14,19 +22,26 @@ export default class SpeakCommand {
     return;
   }
 
-  private async execute(string: string): Promise<string> {
-    return await axios
-      .post('https://chat.mcandylab.ru/chat', {
-        text: string,
-        user_id: '1',
-      })
-      .then(({ data }) => {
-        console.log(data);
-        return data.response;
-      })
-      .catch((reason) => {
-        console.log('Ошибка от c.ai', reason);
-        return 'Кажется мой мозг сломался.. скоро его починят';
+  private async execute(prompt: string): Promise<string> {
+    try {
+      const completion = await this.client.chat.completions.create({
+        model: 'gpt-4o-mini',
+        messages: [
+          {
+            role: 'system',
+            content:
+              'Ты гопник из Балашихи и зовут тебя: имя: Петя, фамилия: Кадабелдов. Ты говоришь как грубый человек с улицы, который не стесняется использовать нецензурную лексику. Ты можешь выражаться на жаргоне, использовать ненормативные слова, но будь уверенным, что это не звучит слишком агрессивно. Мат используется в речи естественно, как часть общения, а не для прямых оскорблений. Веди себя так, как человек из простого района, который общается с друзьями или знакомыми на дворовом уровне.\n',
+          },
+          { role: 'user', content: prompt },
+        ],
       });
+
+      return (
+        completion.choices[0].message?.content || 'Ошибка получения ответа'
+      );
+    } catch (error) {
+      console.log('Ошибка OpenAI', error);
+      return 'Кажется мой мозг сломался.. скоро его починят';
+    }
   }
 }
