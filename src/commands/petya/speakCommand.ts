c1de1f9d8de53b397e8e258599e37371eeff9680
Andrey Abramov <an.abramov@internet.ru>
2024-12-20 11:03:33 +0300

    Подключил новый AI

--- src/commands/petya/speakCommand.ts
+++ src/commands/petya/speakCommand.ts
@@ -2,12 +2,6 @@ import { Context } from 'telegraf';
 import axios from 'axios';
 
 export default class SpeakCommand {
-  private readonly apiKey: string;
-
-  constructor() {
-    this.apiKey = process.env.AI_KEY || '';
-  }
-
   public async init(context: Context, command: string): Promise<void> {
     if (!command) {
       await context.reply('Шо');
@@ -19,25 +13,17 @@ export default class SpeakCommand {
     return;
   }
 
-  private async execute(prompt: string): Promise<string> {
+  private async execute(text: string): Promise<string> {
     try {
       const response = await axios.request({
         method: 'POST',
-        url: 'https://api.chai-research.com/v1/chat/completions',
-        headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
-          'X-API_KEY': this.apiKey,
-        },
+        url: 'https://democratic-daisie-logbucket-f6151baf.koyeb.app',
         data: {
-          model: 'chai_v1',
-          messages: [{ role: 'user', content: prompt }],
+          text,
         },
       });
 
-      return (
-        response.data.choices[0].message?.content || 'Ошибка получения ответа'
-      );
+      return response.data;
     } catch (error) {
       console.log('Ошибка OpenAI', error);
       return 'Кажется мой мозг сломался.. скоро его починят';
